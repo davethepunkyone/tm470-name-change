6e1ed60d7696a3dfebdb3e6cac34d4f19dc03564
Dave <davethepunkyone@hotmail.com>
2020-07-07 08:31:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tm470-name-change',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Dave',
+    author_email='',
+    description=''
+)
