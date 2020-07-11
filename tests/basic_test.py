0a37baa6f18beeab1942f348e467f55069e13878
Dave <davethepunkyone@hotmail.com>
2020-07-11 12:02:45 +0100

    Test build fails with failing test

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class MyBasicTest(unittest.TestCase):
     def test_one_equals_one(self):
-        value_to_check = 1
+        value_to_check = 2
         self.assertEqual(value_to_check, 1)
 
 
