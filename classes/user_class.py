635d309a74fab4d0707f5070de02c885888203e2
Dave <davethepunkyone@hotmail.com>
2020-07-12 15:18:10 +0100

    Did some work with user class

--- classes/user_class.py
+++ classes/user_class.py
@@ -3,22 +3,65 @@ class User:
     """This is a class for the User object.
 
     This contains the following parameters:
-    - ID
+    - User ID
     - First Name
     - Surname
     - Email Address
     - Verified State"""
 
     def __init__(self):
-        self._id = None
+        self._user_id = None
         self._first_name = None
-        self._last_name = None
+        self._surname = None
         self._email = None
         self._verified_state = None
 
-    def set_first_name(self, first_name):
-        self._first_name = first_name
+    @property
+    def user_id(self) -> int:
+        return self._user_id
 
-    def get_first_name(self):
+    @user_id.setter
+    def user_id(self, user_id) -> None:
+        try:
+            id_to_use = int(user_id)
+            self._user_id = id_to_use
+        except ValueError:
+            if user_id is None:
+                self._user_id = None
+            else:
+                raise ValueError("ID provided ({0}) is not numeric or None".format(user_id))
+
+    @property
+    def first_name(self) -> str:
         return self._first_name
 
+    @first_name.setter
+    def first_name(self, first_name) -> None:
+        self._first_name = first_name
+
+    @property
+    def surname(self) -> str:
+        return self._surname
+
+    @surname.setter
+    def surname(self, surname) -> None:
+        self._surname = surname
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @email.setter
+    def email(self, email) -> None:
+        self._email = email
+
+    @property
+    def verified_state(self) -> bool:
+        return self._verified_state
+
+    @verified_state.setter
+    def verified_state(self, verified_state) -> None:
+        if verified_state not in (1, 0, False, True, None):
+            raise ValueError("Verified State provided ({0}) is not boolean or None".format(verified_state))
+        else:
+            self._verified_state = verified_state
--- tests/user_class_test.py
+++ tests/user_class_test.py
@@ -1,19 +1,74 @@
 import unittest
 from classes.user_class import User
 
+test_id = 100
+test_first_name = "Testy"
+test_surname = "McTesterson"
+test_email = "testymctesterson@test.com"
+test_state = True
+
 
 class UserClassTest(unittest.TestCase):
-    def test_get_first_name(self):
-        test_string = "Test"
-        test_user_class = User()
-        test_user_class._first_name = test_string
-        self.assertEqual(test_user_class.get_first_name(), test_string)
+    def setUp(self) -> None:
+        self.test_user = User()
+        self.test_user.user_id = test_id
+        self.test_user.first_name = test_first_name
+        self.test_user.surname = test_surname
+        self.test_user.email = test_email
+        self.test_user.verified_state = test_state
 
-    def test_set_first_name(self):
-        test_string = "Test"
-        test_user_class = User()
-        test_user_class.set_first_name(test_string)
-        self.assertEqual(test_user_class._first_name, test_string)
+    def test_get_id(self) -> None:
+        self.assertEqual(self.test_user.user_id, test_id)
+
+    def test_set_id(self) -> None:
+        different_id = 999
+        self.test_user.user_id = different_id
+        self.assertEqual(self.test_user.user_id, different_id)
+
+    def test_set_invalid_id(self) -> None:
+        different_id = "hello"
+        with self.assertRaises(ValueError) as err:
+            self.test_user.user_id = different_id
+        self.assertEqual(str(err.exception), "ID provided ({0}) is not numeric or None".format(different_id))
+        self.assertEqual(self.test_user.user_id, test_id)
+
+    def test_get_first_name(self) -> None:
+        self.assertEqual(self.test_user.first_name, test_first_name)
+
+    def test_set_first_name(self) -> None:
+        different_name = "Adam"
+        self.test_user.first_name = different_name
+        self.assertEqual(self.test_user.first_name, different_name)
+
+    def test_get_surname(self) -> None:
+        self.assertEqual(self.test_user.surname, test_surname)
+
+    def test_set_surname(self) -> None:
+        different_name = "Jones"
+        self.test_user.surname = different_name
+        self.assertEqual(self.test_user.surname, different_name)
+
+    def test_get_email(self) -> None:
+        self.assertEqual(self.test_user.email, test_email)
+
+    def test_set_email(self) -> None:
+        different_email = "test@newemail.co.uk"
+        self.test_user.email = different_email
+        self.assertEqual(self.test_user.email, different_email)
+
+    def test_get_verified_state(self) -> None:
+        self.assertEqual(self.test_user.verified_state, test_state)
+
+    def test_set_verified_state(self) -> None:
+        different_state = False
+        self.test_user.verified_state = different_state
+        self.assertEqual(self.test_user.verified_state, different_state)
+
+    def test_set_invalid_verified_State(self) -> None:
+        different_state = 2
+        with self.assertRaises(ValueError) as err:
+            self.test_user.verified_state = different_state
+        self.assertEqual(str(err.exception), "Verified State provided ({0}) is not boolean or None".format(different_state))
 
 
 if __name__ == '__main__':
