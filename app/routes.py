6b6158f9c5574aeb7309cfab4d37864eb1f5e71e
Dave <davethepunkyone@hotmail.com>
2020-07-28 10:56:26 +0100

    Amended css, created account home

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,12 @@ def index():
     return render_template('index.html', user=user)
 
 
+@app.route('/account')
+def account_home():
+    user = {"username": "dave"}
+    return render_template('account_home.html', user=user)
+
+
 @app.route('/test')
 def test():
     return "Test Link!"
