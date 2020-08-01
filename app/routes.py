7e8998ba41f2b4a57c34a41ee65b69bf72de7434
Dave <davethepunkyone@hotmail.com>
2020-08-01 10:56:11 +0100

    added rerouting, sorted navigation

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,29 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
+
+user = {"username": "test@test.com",
+        "logged_in": True}
+
+nav = {"show_manage_documents": True,
+       "show_create_access_code": True,
+       "show_manage_access_code": True}
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"username": "dave"}
-    return render_template('index.html', user=user)
+    if user["logged_in"]:
+        return redirect(url_for('account_home'))
+    else:
+        return render_template('index.html', user=user)
+
+
+@app.route('/test')
+def test():
+    return "Test Link!"
+
+
+# New Account Processes
 
 
 @app.route('/new_account_signup')
@@ -19,12 +36,20 @@ def new_account_click_link():
     return render_template('new_account/new_account_signup_clicklink.html')
 
 
+# Existing Account Processes
+
+
 @app.route('/account')
 def account_home():
-    user = {"username": "dave"}
-    return render_template('account_home.html', user=user)
+    return render_template('account_home.html', user=user, nav=nav)
 
 
-@app.route('/test')
-def test():
-    return "Test Link!"
+# New Document Processes
+
+
+@app.route('/new_document_1')
+def new_document_selection():
+    return render_template('add_document/add_doc_1_selection.html', user=user, nav=nav)
+
+
+
